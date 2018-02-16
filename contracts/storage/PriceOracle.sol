222286f09f27e12124b2588ce4d2bc552dcf280a
Art Peel <foundart@gmail.com>
2018-02-16 10:16:48 -0800

    use scaling when handling price discount
    to avoid truncation of discount rate to 0

--- contracts/storage/PriceOracle.sol
+++ contracts/storage/PriceOracle.sol
@@ -13,6 +13,7 @@ import "../base/ArrayHelper.sol";
  */
 contract PriceOracle is Owned, Allowed, ArrayHelper {
     int public assetMultiplier = 10 ** 9;
+    uint64 constant discountRateScale = 10 ** 17;
     mapping(address => uint) public values;
     mapping(address => uint) public lastUpdatedAtBlock;
     address[] public assets;
@@ -78,8 +79,8 @@ contract PriceOracle is Owned, Allowed, ArrayHelper {
      * @param srcAsset The address of the asset to query
      * @param srcAssetAmount The amount in base units of the asset
      * @param targetAsset The asset in which we want to value srcAsset
-     * @param targetDiscountRate the numerator for percentage discount to be applied to current PriceOracle price of targetAsset
-     *
+     * @param targetDiscountRate the unscaled numerator for percentage discount to be applied to current PriceOracle
+     * price of targetAsset (aka for a discount of 5% it should be 5)
      * @return value The value in wei of the asset, or zero.
      */
     function getConvertedAssetValueWithDiscount(address srcAsset, uint256 srcAssetAmount, address targetAsset, uint8 targetDiscountRate) public view returns(uint) {
@@ -88,18 +89,25 @@ contract PriceOracle is Owned, Allowed, ArrayHelper {
             return srcAssetAmount;
         }
 
-        uint srcValue = values[srcAsset];
-        uint targetValue = values[targetAsset];
+        uint scaledSrcValue = scaledDiscountPrice(values[srcAsset], 0);
+        uint scaledTargetValue = scaledDiscountPrice(values[targetAsset], targetDiscountRate);
 
-        if(targetDiscountRate > 0) {
-            targetValue = targetValue * (1 - targetDiscountRate/100);
-        }
-
-        if (srcValue == 0 || targetValue == 0) {
+        if (scaledSrcValue == 0 || scaledTargetValue == 0) {
             return 0; // not supported
         }
 
-        return (srcValue * srcAssetAmount) / targetValue;
+        return (scaledSrcValue * srcAssetAmount) / scaledTargetValue;
+    }
+
+    /**
+      * @notice `scaledDiscountPrice`
+      * @param price value from PriceOracle for an asset
+      * @param unscaledDiscountRate the numerator of a percentage fraction, e.g for 5% it would be 5. 0 is a valid discount.
+      * @return the scaled discount price after applying the discount rate
+      */
+    function scaledDiscountPrice(uint price, uint8 unscaledDiscountRate) public pure returns (uint) {
+
+        return (( 100*discountRateScale - (unscaledDiscountRate * discountRateScale)) * price) / 100;
     }
 
     /**
