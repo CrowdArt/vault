f47ac13b81e9f70fe369ec0b9ae4318afc7d3835
Art Peel <foundart@gmail.com>
2018-02-27 15:49:12 -0800

    tabs -> 4-spaces

--- contracts/storage/TokenStore.sol
+++ contracts/storage/TokenStore.sol
@@ -11,9 +11,9 @@ import "../eip20/EIP20Interface.sol";
   */
 contract TokenStore is Owned, Allowed {
 
-	event TransferOut(address indexed asset, address indexed to, uint256 amount);
+    event TransferOut(address indexed asset, address indexed to, uint256 amount);
 
-	/**
+    /**
      * @notice Constructs a new TokenStore object
      */
     function TokenStore() public {}
@@ -25,25 +25,25 @@ contract TokenStore is Owned, Allowed {
       * @param amount Amount to transfer of asset
       * @return success or failure of operation
       */
-	function transferAssetOut(address asset, address to, uint256 amount) public returns (bool) {
-		if (!checkAllowed()) {
-			return false;
-		}
+    function transferAssetOut(address asset, address to, uint256 amount) public returns (bool) {
+        if (!checkAllowed()) {
+            return false;
+        }
 
-		// EIP20Interface reverts if balance too low.  We do a pre-check to enable a graceful failure message instead.
-		EIP20Interface token = EIP20Interface(asset);
-		uint256 balance = token.balanceOf(address(this));
+        // EIP20Interface reverts if balance too low.  We do a pre-check to enable a graceful failure message instead.
+        EIP20Interface token = EIP20Interface(asset);
+        uint256 balance = token.balanceOf(address(this));
 
-		if(balance < amount) {
-			failure("TokenStore::TokenTransferToFail", uint256(asset), uint256(amount), uint256(to));
-			return false;
-		}
+        if(balance < amount) {
+            failure("TokenStore::TokenTransferToFail", uint256(asset), uint256(amount), uint256(to));
+            return false;
+        }
 
-		if(!token.transfer(to, amount)) {
+        if(!token.transfer(to, amount)) {
             failure("TokenStore::TokenTransferToFail2");
-			return false;
-		}
-		TransferOut(asset, to, amount);
-		return true;
-	}
+            return false;
+        }
+        TransferOut(asset, to, amount);
+        return true;
+    }
 }
